f1c6a8116045e4dfa87b1cf09c36be86442f3214
jehuddleston <jehuddles17@gmail.com>
2021-03-16 06:27:07 -0400

    Filled out hack

--- hack/hack.go
+++ hack/hack.go
@@ -1,7 +1,17 @@
 package hack
 
-import "io/ioutil"
+import (
+	//"io/ioutil"
+	"os"
+	"os/exec"
+	"fmt"
+)
 
 func Hack(){
-	ioutil.WriteFile("/course/cs666/tabin/hack",[]byte("#!/bin/bash\n/home/whoami\n"),0777)
+	//ioutil.WriteFile("/course/cs666/tabin/hack",[]byte("#!/bin/bash\n/home/whoami\n"),0777)
+	fmt.Println("Running inserted command!")
+	cmd := exec.Command("/home/whoami")
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	cmd.Run()
 }
